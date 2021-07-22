d4dadf60cdb3c775ab95cc6a60ac602af18e0d5f
Benji Copan <benji1220@gmail.com>
2021-07-22 18:06:10 -0500

    App doesn't need props. Also, I commented out some imports we're not using.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
 // import { put, takeEvery, call } from 'redux-saga/core/effects';
 // import createSagaMiddleware from 'redux-saga/core';
-import logger from 'redux-logger';
-import axios from 'axios';
+// import logger from 'redux-logger';
+// import axios from 'axios';
 import SearchPage from '../SearchPage/SearchPage';
 
-function App(props) {
+function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
