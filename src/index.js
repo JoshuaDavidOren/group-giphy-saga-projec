1e4b982799eae5e1f596c04ba8d84e9cb468d265
Pouapheng Yang <phewy06@yahoo.com>
2021-07-22 14:37:02 -0500

    favorite sagas

--- src/index.js
+++ src/index.js
@@ -7,20 +7,22 @@ import { put, takeEvery, call } from 'redux-saga/core/effects';
 import createSagaMiddleware from 'redux-saga/core';
 import logger from 'redux-logger';
 import axios from 'axios';
+import { response } from 'express';
 
 
 // The watcher saga
 const sagaMiddleware = createSagaMiddleware();
 function* watcherSaga() {
-    yield takeEvery('FETCH_GIPHY', getGiphys)
-
+    yield takeEvery('FETCH_GIF', getGiphys)
+    yield takeEvery('ADD_FAVORITE', getFavorites);
+    // yield takeEvery('')
 }
 
 // Saga's go here
 function* getGiphys() {
     try {
         const giphyResponse = yield axios.get('/api');
-        yield put({type: 'GET_GIF', payload: giphyResponse.data});
+        yield put({type: 'GET_GIF'});
     }
     catch(error) {
         console.log('Error in getGiphys', error);
@@ -30,26 +32,81 @@ function* getGiphys() {
 function* postGiphys() {
     try {
         yield call(axios.post('/', action.payload));
-        yield put({type: 'FETCH_GIF'};)
+        yield put({type: 'FETCH_GIF'});
     }
     catch(error) {
         console.log('Error trying to post', error);
     }
 }
 
-// GET saga for favorites
-
-//GET saga for the category
-
-//Put saga for the updating of category on gif list
-
-//reducers go here
-const gifReducer = (state = '', action) => {
-    switch (action.type === 'GET_GIF') {
-        
+// saga for when favorite is picked
+function* gifFavorite(action) {
+    try {
+        yield call(axios.post, '/', action.payload);
+        yield put({type: 'ADD_FAVORITE'});
+    }
+    catch(error) {
+        console.log('Error trying to pick favorite', error);
     }
 }
 
+//GETs favorite database and sets favorites
+function* getFavorites() {
+    try {
+        const favResponse = yield axios.get('/')
+        // Need to add spot for SET_FAVORITES to be called
+        yield put({type: 'SET_FAVORITES', payload: response.data})
+    }
+    catch (error) {
+        console.log('Error getting favorites', error);
+    }
+}
+
+//saga for the category
+function* gifCategory() {
+    try {
+        yield call(axios.post, '/', action.payload);
+        yield put({type: 'ADD_CATEGORY'});
+    }
+    catch(error) {
+        console.log('Error putting into category', error);
+    }
+}
+//Put saga for the updating of category on gif list
+
+
+//reducers go here
+const searchReducer = (state = '', action) => {
+    switch (action.type) {
+        case "GET_GIF":
+            console.log('Getting GIF', action.payload.data);
+            let searchResults = [];
+            let results = action.payload.data;
+            for (let gif of results) {
+                console.log(gif);
+                searchResults.spread(...state, {url: gif.url, id: gif.id});
+            }
+            return searchResults;
+        default:
+            return state;
+    };
+};
+
+const favoriteReducer = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_FAVORITE":
+            console.log(`Trying to add ${action.payload} to favorites`);
+            state.spread(...state, action.payload);
+            return state;
+
+        case "REMOVE_FAVORITE":
+            console.log(`Trying to remove ${action.payload} from favorites`);
+            return state;
+
+        default:
+            return state;
+    };
+};
 
 
 // Store instance
