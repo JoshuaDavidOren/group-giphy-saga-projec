57870c4f9914a0ac463d315d75aa5d4cb1e3f319
Sagethh <tate.feige@gmail.com>
2021-07-22 10:42:21 -0500

    added some comments

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -4,22 +4,26 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 // return all favorite images
+// GET data from favorites table
 router.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
 // add a new favorite
+// POST gif to favorites table
 router.post('/', (req, res) => {
   res.sendStatus(200);
 });
 
 // update given favorite with a category id
+// PUT category ID into favorites table
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
   res.sendStatus(200);
 });
 
 // delete a favorite
+// delete from favorites table
 router.delete('/', (req, res) => {
   res.sendStatus(200);
 });
