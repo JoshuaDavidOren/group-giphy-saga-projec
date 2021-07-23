c095dcab452665f87db1df3bb5328789a383f9e0
Sagethh <tate.feige@gmail.com>
2021-07-23 10:28:59 -0500

    added DOM elements to favoritepage, favoriteitem, and added history pushing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import SearchPage from '../SearchPage/SearchPage';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import FavoritePage from '../FavoritePage/FavoritePage';
 
 function App() {
   return (
     <div>
       <h1>Giphy Search!</h1>
-      <SearchPage />
+      <Router>
+      <Route path="/" exact component={SearchPage} />
+      <Route path="/favorites" exact component={FavoritePage}/>
+      </Router>
     </div>
   );
 }
--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
-import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
-function FavoriteItem(item) {
+function FavoriteItem(favItem) {
     const [favoriteItem, setFavoriteItem] = useState([]);
     const classes = useStyles();
     const useStyles = makeStyles((theme) => ({root: {flexGrow: 1},paper: {padding: theme.spacing(2), textAlign: "center", color: theme.palette.text.secondary}})); // materialUI stuff
@@ -15,25 +14,35 @@ function FavoriteItem(item) {
         console.log('Adding to favorites', item);
         dispatch({
             type: 'REMOVE_FAVORITE',
-            payload: item.url
+            payload: item
         });
     };
 
     return (
-        <Grid item style={{ maxWidth: "800px", height: "400px" }} id={item.id}>  
-          <Paper className={classes.paper}>
-            <img src={item.url} />
-            <br />
-            <Button
-              style={{ width: "170px", height: "42px" }}
-              variant="contained"
-              color="primary"
-              onClick={removeFavorite(item)}
-            >
-              Remove From Favorites
-            </Button>
-          </Paper>
-        </Grid>
+      <Grid item style={{height: "350px" }} id={favItem.id}> 
+      <Card>
+        <Paper className={classes.paper}>
+        <CardMedia
+        className={favItem.title}
+        style = {{ height: 250}}
+        component="img"
+        alt={favItem.title}
+        height="140"
+        src={favItem.url}
+        title={favItem.title}
+      />
+          <br />
+          <Button
+            style={{ width: "180px", height: "42px" }}
+            variant="contained"
+            color="primary"
+            onClick={() => removeFavorite(item)}
+          >
+            Remove From Favorites
+          </Button>
+        </Paper>
+        </Card>
+      </Grid>
     );
 };
 
